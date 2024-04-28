3af8403068b6850ed19ba39aa1d66a0dc481a7d2
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2024-04-28 17:50:21 -0500

    mock up log in screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import "tailwindcss/tailwind.css";
 
 function App() {
   return (
-    <div className="bg-tan">
-      <h1 className="font-bold underline">HEY KITTY KAT</h1>
-      <h2>THIS IS MEKOCOLOR</h2>
+    <div className="flex flex-col items-center justify-center space-y-8 m-auto">
+      <h1 className="font-sans font-bold text-2xl">MEKO COLOR</h1>
+      <img
+        className="max-w-36 border-2 border-black shadow-sm"
+        src="/catface.jpg"
+      ></img>
+      <h2 className="font-sans font-bold text-2xl">PASSWORD?</h2>
+      <form className="flex flex-col justfy-between">
+        <input className="border-2 border-black shadow-sm" type="password" />
+        <button className="ml-auto bg-amber-400 border-2 border-black shadow-sm mt-4 p-1 px-2 text-xs font-body">
+          LOGIN
+        </button>
+      </form>
     </div>
   );
 }
