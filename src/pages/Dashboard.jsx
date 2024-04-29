871cdc735d521a9c6d23afc85b91c1a4818ae4f1
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2024-04-29 17:47:23 -0500

    update routing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Dashboard() {
-  return <h1>HI THERE</h1>;
+  return (
+    <div className="">
+      <h1>HI THERE</h1>
+      <button className="ml-auto bg-amber-400 border-2 border-black shadow-sm mt-4 p-1 px-2 text-xs font-body">
+        <Link to="/Clients">CLIENTS HERE</Link>
+      </button>
+    </div>
+  );
 }
