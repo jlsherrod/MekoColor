7584855e2403b333a0a8f4355722092fa4697aff
jlsherrod <74885038+jlsherrod@users.noreply.github.com>
2024-04-29 17:57:42 -0500

    adding buttons to get around for now

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Clients() {
   return (
     <div>
       <h1>AND THIS IS WHERE THE CLIENT INFO GOES...</h1>
+      <button className="ml-auto bg-amber-400 border-2 border-black shadow-sm mt-4 p-1 px-2 text-xs font-body">
+        <Link to="/">HEAD ON HOME</Link>
+      </button>
     </div>
   );
 }
